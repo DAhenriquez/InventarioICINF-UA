859b0d9021e681cde386c39eab7d99a4ce1df45f
DAhenriquez <dahenriquez.n@gmail.com>
2025-10-14 22:46:03 -0300

    update images

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -4,6 +4,7 @@ import './Home.css'
 import './Componentes.css'
 import { Link } from "react-router";
 import list_users from "./json_files/list_users";
+import user from "../images/Componentes/masculino.png"
 
 function Usuarios(){
 
@@ -31,7 +32,7 @@ function Usuarios(){
         <main>
             <aside className="menu-lateral">
                 <div className="user-info">
-                    <Image src="/src/images/masculino.png" alt="User Icon" h={80} w={"auto"} radius="md"></Image>
+                    <Image src={user} alt="User Icon" h={80} w={"auto"} radius="md"></Image>
                     <Text c="#602f09" size="lg" weight={500} mt="sm">Usuario</Text>
                     <Badge color="brown" variant="light" mt="xs">Rol</Badge>
                 </div>
